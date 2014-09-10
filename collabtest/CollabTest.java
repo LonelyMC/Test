54878dcf54618902ee16596b7fdf98610b5387e0
LonelyMC <robert.manzano@gmail.com>
2014-09-10 15:11:06 -0500

    My changes!

--- collabtest/CollabTest.java
+++ collabtest/CollabTest.java
@@ -16,7 +16,9 @@ public class CollabTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int changes = 41;
+        
+        System.out.println(changes++);
     }
     
 }
